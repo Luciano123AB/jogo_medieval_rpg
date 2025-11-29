32e85aabd4af18f8870d324d92e2fd3ad63b73f4
Luciano Eduardo <luciano123ab@gmail.com>
2025-11-29 18:45:01 -0300

    010(Melhorias)

--- resources/views/layouts/direitos.blade.php
+++ resources/views/layouts/direitos.blade.php
@@ -1,7 +1,7 @@
 @php
             
-    $icones = ["laravel", "php", "html5", "javascript", "css", "bootstrap", "animate_css", "sweetalert", "mysql"];
-    $links = ["laravel.com", "php.net", "developer.mozilla.org/en-US/docs/Glossary/HTML5", "developer.mozilla.org/pt-BR/docs/Web/JavaScript", "developer.mozilla.org/pt-BR/docs/Web/CSS", "getbootstrap.com", "animate.style", "sweetalert2.github.io", "mysql.com"];
+    $icones = ["laravel", "php", "html5", "javascript", "css", "bootstrap", "animate_css", "jsdelivr", "sweetalert", "mysql"];
+    $links = ["laravel.com", "php.net", "developer.mozilla.org/en-US/docs/Glossary/HTML5", "developer.mozilla.org/pt-BR/docs/Web/JavaScript", "developer.mozilla.org/pt-BR/docs/Web/CSS", "getbootstrap.com", "animate.style", "cdn.jsdelivr.net", "sweetalert2.github.io", "mysql.com"];
 
 @endphp
 
@@ -14,7 +14,7 @@
             </span>
         </small>
     </div>
-    <div class="d-flex gap-3 justify-content-center py-1 overflow-x-auto">
+    <div class="d-flex gap-3 justify-content-center py-2 overflow-x-auto">
         @foreach(array_combine($icones, $links) as $icone => $link)
             <a href="http://{{ $link }}">
                 <img src="{{ asset("assets/images/icones/$icone.png") }}" class="cursor icones botoes">
