e968e2e897122198dc336e57374bb3f6b0495c86
Luciano Eduardo <luciano123ab@gmail.com>
2025-12-19 17:53:22 -0300

    010(Melhorias)

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -59,8 +59,10 @@
 
         @if(session()->has("player") && $pagina != "Listagem" && $pagina != "Batalha")
             <a href="{{ route("listagem") }}" class="cursor sombras botoes animate__animated animate__fadeIn btn btn-lg {{ session("tema") == "escuro" ? "btn-secondary border-primary focus-ring focus-ring-primary" : "btn-dark border-danger focus-ring focus-ring-danger" }} d-flex border my-1">
-                <span class="cursor {{ session("tema") == "escuro" ? "cor_fontes_escuro" : "cor_fontes_claro" }}">
-                    <i class="cursor bi bi-list-stars"></i>
+                <span class="cursor {{ session("tema") == "escuro" ? "cor_fontes_escuro" : "cor_fontes_claro" }} d-flex justify-content-center">
+                    <div class="cursor animate__animated animate__flipInX animate__infinite">
+                        <i class="cursor bi bi-list-stars me-2"></i>
+                    </div>
                     Lista de Players/Rank
                 </span>
             </a>
