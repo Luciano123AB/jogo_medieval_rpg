38dfe0e644b65c8a8c212c6c5ddb222d38ecc1f6
Luciano123AB <Lucianoedustefa24032004@gmail.com>
2026-03-18 16:29:17 -0300

    Fix include statement for scripts in main layout

--- resources/views/layouts/main_layout.blade.php
+++ resources/views/layouts/main_layout.blade.php
@@ -24,7 +24,7 @@
 
     @include("layouts.direitos")
 
-    @include("layouts.partials.scripts.scripts");
+    @include("layouts.partials.scripts.scripts")
 
     <img src="{{ asset('assets/images/gifs/' . (session('tema') == 'escuro' ? 'fogo_invertido.gif' : 'fogo.gif')) }}" class="gifs position-fixed bottom-0 start-50 translate-middle-x opacity-25 w-100 h-25">
 </body>
